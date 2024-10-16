af2dec8a6e8823ded85c40db86506c6cb0f6df12
sebastianrealcalderon <u20221d964@upc.edu.pe>
2024-10-15 19:28:12 -0500

    feat: added transaction entity attributes

--- src/app/shared/model/transaction.entity.ts
+++ src/app/shared/model/transaction.entity.ts
@@ -1,2 +1,26 @@
 export class Transaction {
+    id: number;
+    transaction_type_id: number;
+    walletId: number;
+    amount: number;
+    date: string;
+    note: string;
+    category_id: number;
+    constructor(
+        id = 0,
+        transaction_type_id = 0,
+        walletId = 0,
+        amount = 0,
+        date = "",
+        note = "",
+        category_id = 0
+    ) {
+        this.id = id;
+        this.transaction_type_id = transaction_type_id;
+        this.walletId = walletId;
+        this.amount = amount;
+        this.date = date;
+        this.note = note;
+        this.category_id = category_id
+    }
 }
